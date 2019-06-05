e0658888e9c2307a234c976f4e42b5095a6a426f
mleumonster <mleu@atlassian.com>
2019-06-05 20:31:24 +0000

    Introduce cli

--- main.go
+++ main.go
@@ -2,70 +2,85 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/go-reverse-proxy/proxy"
 	"github.com/sirupsen/logrus"
+	"gopkg.in/urfave/cli.v1"
 	"gopkg.in/yaml.v2"
 )
 
-const FAILED_TIMEOUT = 15
-
-type override struct {
-	Header string
-	Match  string
-	Host   string
-	Path   string
-}
-
-// Upstream represents upstream host
-type Upstream struct {
-	Active      bool
-	Host        string
-	LastAttempt int32
-	LastFailure int32
-}
-
-// Service represents the backend service
-type Service struct {
-	Name      string
-	Upstreams []Upstream
-	Port      string
-	Override  override
-	Proxy     http.Handler
-}
-
-// UpstreamConfig represents all configuration of upstream service
-type UpstreamConfig struct {
-	Services []Service
-}
+const (
+	defaultPort = 80
+)
 
 func main() {
-	logger, err := configureLogger("text")
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
-	upstreamConfig, err := loadConfig("/opt/go/src/github.com/go-reverse-proxy/upstream.yaml")
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
-	rr, err := proxy.NewReverseRouter(
-		proxy.UseUpstreamConfig(upstreamConfig),
-		proxy.UseLogger(logger),
+	var (
+		logFormat  string
+		bind       string
+		configFile string
 	)
 
-	if err != nil {
-		logrus.Fatalln(err)
+	app := cli.NewApp()
+	app.Version = "Go ReverseProxy version 0.1"
+	app.Usage = "A general purpose proxy service"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "lf,log-format",
+			Destination: &logFormat,
+			Usage:       "--log-format=json can only use json or text",
+			EnvVar:      "LOG_FORMAT",
+			Value:       "text",
+		},
+		cli.StringFlag{
+			Name:        "b, bind",
+			Destination: &bind,
+			EnvVar:      "BIND",
+			Value:       fmt.Sprintf(":%d", defaultPort),
+		},
+		cli.StringFlag{
+			Name:        "c, config",
+			Destination: &configFile,
+			EnvVar:      "CONFIG",
+			Value:       "/opt/go/src/github.com/go-reverse-proxy/upstream.yaml",
+		},
 	}
 
-	logger.Infof("go-reverse-proxy - running on 80, pid: %d", os.Getpid())
-	log.Fatal(http.ListenAndServe(":80", rr))
+	app.Action = func(c *cli.Context) error {
+		logger, err := configureLogger(logFormat)
+		if err != nil {
+			return err
+		}
+
+		upstreamConfig, err := loadConfig(configFile)
+		if err != nil {
+			return err
+		}
+
+		rr, err := proxy.NewReverseRouter(
+			proxy.UseUpstreamConfig(upstreamConfig),
+			proxy.UseLogger(logger),
+		)
+
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+
+		logger.Infof("go-reverse-proxy - running on 80, pid: %d", os.Getpid())
+		if err := http.ListenAndServe(bind, rr); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		os.Exit(1)
+	}
 }
 
 // loadConfig loads the configurations of upstream services from .yaml config file
